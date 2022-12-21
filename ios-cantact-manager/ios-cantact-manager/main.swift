9c49781db60c4769da2944e8bafb0031ef4e9892
CODEMASTERJO <ekrktk1221@naver.com>
2022-12-21 13:29:01 +0900

    feat. 이름, 나이, 연락처 에러처리 함수 추가

--- ios-cantact-manager/ios-cantact-manager/main.swift
+++ ios-cantact-manager/ios-cantact-manager/main.swift
@@ -10,7 +10,7 @@ import Foundation
 enum ValidInputError: String, Error {
     case notValidName = "이름"
     case notValidAge = "나이"
-    case notValidContect = "연락처"
+    case notValidContact = "연락처"
 }
 
 // 정규표현식 충족 여부
@@ -22,5 +22,20 @@ func errorMessage(_ data: String) -> String {
     "입력한 \(data) 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
 }
 
+func checkName(_ name: String) throws -> String {
+    guard !regexMatch else { return name }
+    print(errorMessage(name))
+    throw ValidInputError.notValidName
+}
 
+func checkAge(_ age: String) throws -> String {
+    guard !regexMatch else { return age }
+    print(errorMessage(age))
+    throw ValidInputError.notValidAge
+}
 
+func checkContact(_ contact: String) throws -> String {
+    guard !regexMatch else { return contact }
+    print(errorMessage(contact))
+    throw ValidInputError.notValidContact
+}
