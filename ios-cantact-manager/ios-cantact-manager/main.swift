13183d4d1fcc72002a2d91716615f56d574d8d01
CODEMASTERJO <ekrktk1221@naver.com>
2022-12-21 13:15:26 +0900

    feat. 에러정의

---
+++ ios-cantact-manager/ios-cantact-manager/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  ios-cantact-manager
+//
+//  Created by 조용현 on 2022/12/21.
+//
+
+import Foundation
+
+enum ValidInputError: String, Error {
+    case notValidName = "이름"
+    case notValidAge = "나이"
+    case notValidContect = "연락처"
+}
