19600c9e8b3b175e5bb68c79770e4755415501eb
zhunhe <xuzhunhe@gmail.com>
2022-12-21 16:19:36 +0900

    style: SwiftLint 추가 및 에러 수정
    - https://github.com/realm/SwiftLint

--- CantactManager/CantactManager/main.swift
+++ CantactManager/CantactManager/main.swift
@@ -7,14 +7,14 @@
 
 import Foundation
 
-func split(input: String, by: String) -> [String] {
-    return input.components(separatedBy: by)
+func split(input: String, separatedBy: String) -> [String] {
+    return input.components(separatedBy: separatedBy)
 }
 
 func run() {
     Message.pleaseInputContactData.printSelf(terminator: "")
     if let input = readLine() {
-        let splitted = split(input: input, by: "/")
+        let splitted = split(input: input, separatedBy: "/")
         guard splitted.count == 3 else {
             Message.inputError.printSelf(terminator: "\n")
             return
