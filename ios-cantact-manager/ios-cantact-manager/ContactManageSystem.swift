785355c3baab206e3f763479ecbe56669e951269
CODEMASTERJO <ekrktk1221@naver.com>
2022-12-22 10:40:11 +0900

    feat. 생성된 연락처 정보를 저장하는 딕셔너리 변수 추가

--- ios-cantact-manager/ios-cantact-manager/ContactManageSystem.swift
+++ ios-cantact-manager/ios-cantact-manager/ContactManageSystem.swift
@@ -9,12 +9,12 @@ import Foundation
 
 struct ContactManageSystem {
     let inputManager = InputManager()
+    var contactList = [String: Profile]()
     
-    func addProfile() {
-
+    mutating func addProfile() {
         let inputArray = inputManager.inputInfo()
         let (name, age, tel) = (inputArray[0], inputArray[1], inputArray[2])
-        let jojo = Profile(name: name, age: age, tel: tel)
-        print(jojo)
+        contactList.updateValue(Profile(name: name, age: age, tel: tel), forKey: name)
+        print(contactList)  // FIXIT: - 테스토용(이후 제거)
     }
 }
