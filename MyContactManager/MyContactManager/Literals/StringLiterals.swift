fc6a0ecb38d948574256a7771d272ca9eebd2d79
devxsby <devxsby@gmail.com>
2022-12-22 14:43:46 +0900

    CHORE: String Literal 변수명 변경

--- MyContactManager/MyContactManager/Literals/StringLiterals.swift
+++ MyContactManager/MyContactManager/Literals/StringLiterals.swift
@@ -7,10 +7,10 @@
 
 import Foundation
 
-struct StringLiterals {
-    static let enterMesssage = "연락처 정보를 입력해주세요 :"
-    static let wrongAgeInputMessage = "입력한 나이정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    static let wrongPhoneNumberInputMessage = "입력한 연락처정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    static let wrongInputMessage = "입력한 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    static let noInputMessage = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
+struct Message {
+    static let greeting = "연락처 정보를 입력해주세요 :"
+    static let wrongAge = "입력한 나이정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+    static let wrongPhoneNumber = "입력한 연락처정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+    static let defaultError = "입력한 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+    static let noUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
 }
