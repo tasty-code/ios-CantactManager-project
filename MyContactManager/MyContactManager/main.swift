441ea7904f9f85c76c353dfff980d8cead363df1
devxsby <devxsby@gmail.com>
2022-12-22 13:48:55 +0900

    FEAT: 사용자에게 3가지 정보 입력 받는지 확인하는 함수 구현

--- MyContactManager/MyContactManager/main.swift
+++ MyContactManager/MyContactManager/main.swift
@@ -20,9 +20,18 @@ class MyContactManager {
     
     private func getUserInput() {
         guard let userInput = readLine() else { return }
-        let inputData = userInput.split(separator: " ")
-        if inputData.isEmpty {
+        let inputData = userInput.components(separatedBy: " ").joined()
+        let input = inputData.split(separator: "/").map { String($0) }
+        
+        if input.count == 3 {
+            let (name, age, number) = (input[0], input[1], input[2])
+            print("name: \(name)")
+            print("age: \(age)")
+            print("number: \(number)")
+        } else if input.isEmpty {
             print(StringLiterals.noInputMessage)
+        } else {
+            print(StringLiterals.wrongInputMessage)
         }
     }
 
