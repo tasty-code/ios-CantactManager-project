1775b11baa79daa8b95ce8dd6e6c4f1fc384f58d
ohdair <ohdiar@gmail.com>
2022-12-22 13:46:53 +0900

    test: 테스트 모듈 추가

---
+++ ContactManager/ContactManagerTest/ContactManagerTest.swift
@@ -0,0 +1,23 @@
+//
+//  ContactManagerTest.swift
+//  ContactManagerTest
+//
+//  Created by 박재우 on 2022/12/22.
+//
+
+import XCTest
+
+final class ContactManagerTest: XCTestCase {
+
+    override func setUpWithError() throws {}
+    override func tearDownWithError() throws {}
+
+    func testSplit() throws {
+        XCTAssertEqual(split(input: "james/30/123-1234-1234"), ["james", "30", "123-1234-1234"])
+    }
+
+    func testPerformanceExample() throws {
+        measure {}
+    }
+
+}
