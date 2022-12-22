6733c6863f2271eaf1df02407785c0782b221315
sloDevz <onec555@gmail.com>
2022-12-22 10:50:51 +0900

    FEAT: PersonManager 클래스 생성, Person 구조체 생성.
    이름 나이와 폰번호를 Person 구조체로 묶음.
    PersonManager 안에 이름과 Person 구조체로 데이터를 묶을 예정.

---
+++ MyContactManager/MyContactManager/Model/Person.swift
@@ -0,0 +1,14 @@
+//
+//  Person.swift
+//  MyContactManager
+//
+//  Created by DONGWOOK SEO on 2022/12/22.
+//
+
+import Foundation
+
+struct Person {
+    let name: String
+    let age: String
+    let phoneNum: String
+}
---
+++ MyContactManager/MyContactManager/Model/PersonManager.swift
@@ -0,0 +1,12 @@
+//
+//  PersonManager.swift
+//  MyContactManager
+//
+//  Created by DONGWOOK SEO on 2022/12/22.
+//
+
+import Foundation
+
+class PersonManager {
+    var contactsDict: [String : Person] = [:]
+}
