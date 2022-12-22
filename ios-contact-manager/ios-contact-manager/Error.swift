65b3dc20c9eb33692f195018119c0fce6fb976a8
Sei Kim <vanism7557@gmail.com>
2022-12-22 14:24:04 +0900

    feat: 문자열 검증 에러 정의 #7

--- ios-contact-manager/ios-contact-manager/Error.swift
+++ ios-contact-manager/ios-contact-manager/Error.swift
@@ -9,14 +9,24 @@ import Foundation
 
 enum IOError: Error {
     case emptyInput
-    
+    case ageError
+    case phoneError
+    case inputFormatError
 }
 
 extension IOError: LocalizedError {
     public var errorDescription: String? {
+        let description:String
         switch self {
         case .emptyInput:
-            return "아무것도 입력되지 않았습니다."
+            description = "아무것도 입력되지 않았습니다."
+        case .ageError:
+            description = "입력한 나이 정보가 잘못되었습니다."
+        case .phoneError:
+            description = "입력한 연락처 정보가 잘못되었습니다."
+        case .inputFormatError:
+            description = "입력 형식이 잘못되었습니다."
         }
+        return "\(description) \(StringLiteral.help)"
     }
 }
