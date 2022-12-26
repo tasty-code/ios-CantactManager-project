23a6108cceae8c6b26b2662e0fec5a49cc581405
lust3r <s1skyblue1s@gmail.com>
2022-12-26 16:01:25 +0900

    FEAT: calculateSlashNum 함수 정의 및 입력값 검증

--- contactManager/contactManager/main.swift
+++ contactManager/contactManager/main.swift
@@ -1,2 +1,21 @@
+import Foundation
+
+func calculateSlashNum(input: String) -> Bool {
+    if (input.filter{ $0 == "/" }.count != 2) {
+        return false
+    }
+    return true
+}
+
 var isError: Bool = true
 let regex = "^[A-Za-z\\s]+$"
+
+print("")
+print("연락처 정보를 입력해주세요: ")
+
+if let input = readLine() {
+    
+    if !(calculateSlashNum(input: input)) {
+        print("입력된 정보가 [이름]/[나이]/[전화번호] 형식인지 확인해주세요")
+    }
+}
