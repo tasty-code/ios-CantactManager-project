692942d5d71fac1e8a8e85dce035c086bb1d368a
toughie97 <airj907@gmail.com>
2022-12-26 15:47:17 +0900

    validInput의 공백제거, /을 기준으로 스플릿, 스플릿된 요소 개수가 3개인지 확인하는 transform 함수 선언. transform 된 리턴값을 transformedInput이라는 빈어레이에 할당

--- ios_contact_manager/ios_contact_manager/main.swift
+++ ios_contact_manager/ios_contact_manager/main.swift
@@ -19,3 +19,13 @@ func inputNewUser() {
         print("아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요.")
         return }
 }
+
+var transformedInput: [String] = []
+
+func transform(input: String) {
+    let spaceRemovedInput = input.replacingOccurrences(of: " ", with: "")
+    let splitedInput = spaceRemovedInput.components(separatedBy: "/")
+    guard splitedInput.count == 3 else {return}
+    
+    transformedInput = splitedInput
+}
