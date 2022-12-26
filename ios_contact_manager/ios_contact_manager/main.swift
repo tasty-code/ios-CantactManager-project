213aa31147a21e1dd4b4e79561c06cba00675916
iYeahh <yboravender@gmail.com>
2022-12-26 15:45:31 +0900

    readLine의 nil, 빈 문자열 체크하여 validInput에 할당

--- ios_contact_manager/ios_contact_manager/main.swift
+++ ios_contact_manager/ios_contact_manager/main.swift
@@ -7,3 +7,15 @@ struct UserInfo {
 }
 
 var allUsersData: [UserInfo] = []
+
+var validInput:String = ""
+
+func inputNewUser() {
+    print("연락처 정보를 입력해주세요 : ")
+    let input = readLine()
+    if let someInput = input, someInput.count > 0 {
+        validInput = someInput
+    }  else {
+        print("아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요.")
+        return }
+}
