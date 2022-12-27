df886f25d632c743b4b982bbf7e8bc8bf72d7c5a
devxsby <devxsby@gmail.com>
2022-12-27 16:22:42 +0900

    CHORE: Errors enum rawValue 출력 방식 변경

--- MyContactManager/MyContactManager/Enum/Errors.swift
+++ MyContactManager/MyContactManager/Enum/Errors.swift
@@ -7,13 +7,22 @@
 
 import Foundation
 
-enum Errors: String, LocalizedError {
-    case wrongAge = "입력한 나이정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    case wrongPhoneNumber = "입력한 연락처정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    case defaultError = "입력한 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
-    case noUserInput = "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
+enum Errors: LocalizedError {
+    case wrongAge
+    case wrongPhoneNumber
+    case noUserInput
+    case defaultError
     
     var errorDescription: String? {
-        return self.rawValue
+        switch self {
+        case .wrongAge:
+            return "입력한 나이정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+        case .wrongPhoneNumber:
+            return "입력한 연락처정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+        case .noUserInput:
+            return "아무것도 입력되지 않았습니다. 입력 형식을 확인해주세요."
+        case .defaultError:
+            return "입력한 정보가 잘못되었습니다. 입력 형식을 확인해주세요."
+        }
     }
 }
