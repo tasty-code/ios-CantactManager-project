7df689f9ff881194634fb3e8c42b2dd2ffac169f
toughie97 <airj907@gmail.com>
2022-12-27 14:20:27 +0900

    <feat>: <Step2 구현> <BLANK LINE> <body> - 앱이 실행중인지 확인하는 변수 isAppRunning 선언<br/> - 앱이 실행되는 동안 반복되는 while문이 포함된 startApp 함수 생성(메뉴 입력)<br/> - 연락처 추가하는 함수 addContact 생성(메뉴: 1)<br/> - 앱을 종료하는 함수 endApp 생성(메뉴: x)<br/>

--- ios_contact_manager/ios_contact_manager/main.swift
+++ ios_contact_manager/ios_contact_manager/main.swift
@@ -74,10 +74,41 @@ func addNewUser() {
     if checkedAge > 0, checkedName != "", checkedPhoneNumber != "" {
         print("입력한 정보는 \(checkedAge)세 \(checkedName)(\(checkedPhoneNumber))입니다.")
         allUsersData.append(UserInfo(name: checkedName, age: checkedAge, phoneNumbers: [checkedPhoneNumber]))
+
     }
 }
 
-inputNewUser()
-transform(input: validInput)
-verifyInput(input: transformedInput)
-addNewUser()
+func addContact() {
+    inputNewUser()
+    transform(input: validInput)
+    verifyInput(input: transformedInput)
+    addNewUser()
+//    return
+}
+
+func endApp() {
+    print("[프로그램 종료]")
+    isAppRunning = false
+}
+
+
+
+var isAppRunning = true
+
+func startApp() {
+    while isAppRunning {
+        print("1) 연락처 추가 2) 연락처 목록보기 3) 연락처 검색 x) 종료\n메뉴를 선택해주세요 : ")
+        let inputMenu = readLine() ?? ""
+        
+        switch inputMenu {
+        case "1": addContact()
+        case "2": continue
+        case "3": continue
+        case "x": endApp()
+        default:
+            print("선택이 잘못되었습니다 확인 후 다시 입력해주세요.")
+        }
+    }
+}
+
+startApp()
