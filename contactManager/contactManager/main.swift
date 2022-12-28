dcdc7fa80fe6eaa6ce9ba717f86bf6c392818bdb
lust3r <s1skyblue1s@gmail.com>
2022-12-28 13:27:16 +0900

    REFACTOR: repeatErrorMessage 변수를 isAddContactError로 변경 후 Bool값을 반대로 변경

--- contactManager/contactManager/main.swift
+++ contactManager/contactManager/main.swift
@@ -49,10 +49,10 @@ func errorCheck(trimName: String, trimAge: String, trimNum: String) -> Bool {
     return true
 }
 
-var repeatErrorMessage: Bool = true
+var isAddContactError: Bool = false
 let regex = "^[A-Za-z\\s]+$"
 
-while repeatErrorMessage {
+while !isAddContactError {
     print("")
     print("연락처 정보를 입력해주세요: ", terminator: "")
     
@@ -77,7 +77,7 @@ while repeatErrorMessage {
     guard errorCheck(trimName: trimName, trimAge: trimAge, trimNum: trimNum) else {
         continue
     }
-    repeatErrorMessage = false
+    isAddContactError = true
     print("입력한 정보는 \(trimAge)세 \(zeroBlankName)(\(trimNum))입니다.")
 
 }
