571634f175a5dd90889956bf70429e434610f9b8
ohdair <ohdiar@gmail.com>
2022-12-30 14:07:09 +0900

    refactor: getLine(), createCommand() 이동

--- ContactManager/ContactManager/ContactManager.swift
+++ ContactManager/ContactManager/ContactManager.swift
@@ -90,4 +90,30 @@ struct ContactManager {
             print(error.localizedDescription)
         }
     }
+
+    func getLine() throws -> String? {
+        guard let input = readLine() else {
+            return nil
+        }
+        guard input.isEmpty == false else {
+            throw ContactManagerError.emptyInput
+        }
+        return input
+    }
+
+    private func createCommand() throws -> Command? {
+        Message.pleaseInputMenu.printSelf()
+        do {
+            guard let input = try getLine() else {
+                return Command.quit
+            }
+            guard let command = Command(rawValue: input) else {
+                throw ContactManagerError.invalidCommand
+            }
+            return command
+        } catch {
+            print(error.localizedDescription)
+        }
+        return nil
+    }
 }
--- ContactManager/ContactManager/main.swift
+++ ContactManager/ContactManager/main.swift
@@ -9,30 +9,4 @@ import Foundation
 
 var manager = ContactManager()
 
-func getLine() throws -> String? {
-    guard let input = readLine() else {
-        return nil
-    }
-    guard input.isEmpty == false else {
-        throw ContactManagerError.emptyInput
-    }
-    return input
-}
-
-func createCommand() throws -> Command? {
-    Message.pleaseInputMenu.printSelf()
-    do {
-        guard let input = try getLine() else {
-            return Command.quit
-        }
-        guard let command = Command(rawValue: input) else {
-            throw ContactManagerError.invalidCommand
-        }
-        return command
-    } catch {
-        print(error.localizedDescription)
-    }
-    return nil
-}
-
 manager.runProgram()
