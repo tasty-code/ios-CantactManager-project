76c1b6c7ebd645ba16971dadf44a7d22d549274e
longlivedrgn <kimye9808@naver.com>
2022-12-30 11:53:40 +0900

    REFACTOR: 메뉴 선택 enum type 생성 및 switch문 수정

--- contactManager/contactManager/main.swift
+++ contactManager/contactManager/main.swift
@@ -17,6 +17,13 @@ enum GuideText {
    static let selectMenu = "메뉴를 선택해주세요 : "
 }
 
+enum MenuType: String {
+    case addContact = "1"
+    case viewContactList = "2"
+    case findContact  = "3"
+    case exit = "x"
+}
+
 func errorTextOutput(with type: ErrorType){
     let errorText: String
     switch type {
@@ -35,30 +42,28 @@ let regex = "^[A-Za-z\\s]+$"
 while !isMenuInputError {
     print(GuideText.menu)
     print(GuideText.selectMenu,terminator: "")
-    guard let input = readLine(), input != "" else{
+    guard let input = readLine(), let menu = MenuType(rawValue: input) else{
+        print("선택이 잘못되었습니다. 확인 후 다시 입력해주세요.\n")
         continue
     }
-    switch input{
-    case "1":
+    switch menu{
+    case .addContact:
         addContact()
-    case "2":
+    case .viewContactList:
         for contact in contacts.sorted(by: {$0.name < $1.name}) {
             print("- \(contact.name) / \(contact.age) / \(contact.phoneNumber)")
         }
         print("")
         continue
-    case "3":
+    case .findContact:
         print("연락처에서 찾을 이름을 입력해주세요 : ", terminator: "")
         guard let inputValue = readLine() else {
             continue
         }
         findContact(inputName: inputValue)
         continue
-    case "x":
+    case .exit:
         isMenuInputError = true
         print("\n[프로그램 종료]")
-    default:
-        print("선택이 잘못되었습니다. 확인 후 다시 입력해주세요.\n")
-        continue
     }
 }
